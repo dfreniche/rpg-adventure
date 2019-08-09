9480a19e5ff2a75e9226146a928d7f4441fb1b29
Diego Freniche Brito <diego@freniche.com>
2019-08-09 20:41:31 +0200

    Using scripts from C version to start RVM, bring to front etc. Also taking into account that now we have two BASIC files

--- basic/make.sh
+++ basic/make.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 IDSK=$CPCT_PATH/tools/iDSK-0.13/bin/iDSK
-SOURCE=rpg.bas
-DESTINATION=rpggame.bas
+SOURCE1=rpg.bas
+SOURCE2=main.bas
+DESTINATION1=rpg-c.bas
+DESTINATION2=main-c.bas
 TARGET=rpg.dsk
-RVM=/Applications/Retro\ Virtual\ Machine\ 2.app/Contents/MacOS/Retro\ Virtual\ Machine\ 2
 PRESENTATION_FILE=present.txt
 
+# Close any open RVM2 instance
+./close-rvw.sh
+
+
 # REMOVE COMMENTS FROM BASIC FILE
-grep -v "'" $SOURCE > $DESTINATION
+grep -v "'" $SOURCE1 > $DESTINATION1
+grep -v "'" $SOURCE2 > $DESTINATION2
 
 # CREATE DSK
 $IDSK $TARGET -n
@@ -16,17 +22,27 @@ $IDSK $TARGET -n
 # Convert to MSDOS CR+LF LINE FORMAT
 # This needs unix2dos util 
 # brew install unix2dos
-unix2dos $SOURCE
-unix2dos $DESTINATION
+unix2dos $SOURCE1
+unix2dos $SOURCE2
+unix2dos $DESTINATION1
+unix2dos $DESTINATION2
 unix2dos $PRESENTATION_FILE
 
 # ADD TO DSK
-$IDSK $TARGET -i $SOURCE -t 0
-$IDSK $TARGET -i $DESTINATION -t 0
+$IDSK $TARGET -i $SOURCE1 -t 0
+$IDSK $TARGET -i $SOURCE2 -t 0
+$IDSK $TARGET -i $DESTINATION1 -t 0
+$IDSK $TARGET -i $DESTINATION2 -t 0
 $IDSK $TARGET -i $PRESENTATION_FILE -t 0
 
 # ONCE MINIMIFIED FILE IS IN DSK, DELETE IT
-rm $DESTINATION
+rm $DESTINATION1
+rm $DESTINATION2
+
+echo "Success 🎉, let's launch RVM 🚀"
 
 # LAUNCH RVM
-"$RVM" -b=cpc6128 -insert $TARGET -c="RUN\"$DESTINATION\n"
+cpct_rvm $TARGET
+
+# BRING RVM TO FRONT
+./bring-rvm-front.sh
