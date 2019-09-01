3a4a8e688d4048c6af8a21e6e609341e88fa99f5
dfreniche <diego@freniche.com>
2019-09-01 18:56:32 +0200

    Added a make-linux script to build from Ubuntu in Windows

---
+++ c/rpg/make-linux.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+IDSK=$CPCT_PATH/tools/iDSK-0.13/bin/iDSK
+TARGET=rpg.dsk
+PRESENTATION_FILE=src/present.txt
+
+# Make will create DSK with the binary
+make
+
+# Check make went OK, if not print message and exit
+if [ $? -ne 0 ]; then
+    echo "..."
+    echo "💥 Error compiling / linking / check above message"
+    exit
+fi
+
+# ADD TO DSK
+$IDSK $TARGET -i $PRESENTATION_FILE -t 0
+
+echo "Success 🎉, let's launch RVM 🚀"
+
+# LAUNCH RVM
+/mnt/c/Users/dfren/Downloads/RetroVirtualMachine.2.0.beta-1.r7.windows.x64/RetroVirtualMachine.exe -insert $TARGET  --boot=cpc6128@es -command="run\"rpg\""
